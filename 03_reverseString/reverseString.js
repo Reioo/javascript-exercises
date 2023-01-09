d9e6163c31e1d9fcc8ed532b9eea56ee43be4b6b
Reioo <reio.meiusi@gmail.com>
2023-01-09 23:57:59 +0200

    loop through string from the end

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(reversable) {
+    let answer = '';
+    for (let i = reversable.length - 1; i >= 0; i--) {
+        answer += reversable.charAt(i);
+    }
+    return answer;
 };
 
 // Do not edit below this line

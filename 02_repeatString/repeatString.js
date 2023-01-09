a6854dc893c89909e8d066a2be4635525d500f59
Reioo <reio.meiusi@gmail.com>
2023-01-09 23:39:38 +0200

    first 2 exercices done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let answer = '';
+    if (num < 0) {
+        answer = 'ERROR';
+    } else {
+    for (let i = 0; i < num; i++) {
+        answer += word; 
+    }
+    }
+    return answer;
 };
 
 // Do not edit below this line

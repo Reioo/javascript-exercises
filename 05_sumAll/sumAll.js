53ae55d253d2a07e14009f63bc53043667e3eee8
Reioo <reio.meiusi@gmail.com>
2023-01-12 18:51:33 +0200

    Switch places if first num is larger, loop through

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    // If number 1 is smaller than number 2, switch places
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    for (num1; num1 <= num2; num1++ ) {
+        sum += num1;
+    }
+    return sum;
 };
 
 // Do not edit below this line

49f97e2c42439d7241794d9dc752b11969fdfe10
Reioo <reio.meiusi@gmail.com>
2023-01-12 18:30:18 +0200

    Add 3 dots to method argument and works

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...element) {
+    let answer = array;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j<element.length; j++) {
+            if (answer.includes(element[j])) {
+                answer.splice(answer.indexOf(element[j]), 1);    
+            }
+        }        
+    }
+    return answer;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;

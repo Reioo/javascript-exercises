df60eb92c19828588c9ee9f6cd388e28604ee1ea
Reioo <reio.meiusi@gmail.com>
2023-01-12 19:33:50 +0200

    Add math.round function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(givenTemperature) {
+  let celsius = 0;
+  var multiplier = Math.pow(10, 1 || 0);
+  celsius = (givenTemperature - 32) * (5/9);
+  console.log('Before rounding' + celsius);
+  celsius = Math.round(celsius * multiplier) / multiplier
+  console.log('After rounding' + celsius);
+  return celsius;
 };
 
 const convertToFahrenheit = function() {

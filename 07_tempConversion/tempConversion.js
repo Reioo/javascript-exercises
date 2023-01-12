5d8ba9782d1e2d424815282b2b9c67c472c4b6ac
Reioo <reio.meiusi@gmail.com>
2023-01-12 19:38:09 +0200

    Make fahrenheit converter

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,13 +2,16 @@ const convertToCelsius = function(givenTemperature) {
   let celsius = 0;
   var multiplier = Math.pow(10, 1 || 0);
   celsius = (givenTemperature - 32) * (5/9);
-  console.log('Before rounding' + celsius);
   celsius = Math.round(celsius * multiplier) / multiplier
-  console.log('After rounding' + celsius);
   return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(givenTemperature) {
+  let fahrenheit = 0;
+  var multiplier = Math.pow(10, 1 || 0);
+  fahrenheit = (givenTemperature * (9/5)) + 32;
+  fahrenheit = Math.round(fahrenheit * multiplier) / multiplier
+  return fahrenheit;
 };
 
 // Do not edit below this line

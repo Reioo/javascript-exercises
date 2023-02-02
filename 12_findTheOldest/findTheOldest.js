69a12d12c05d22fafbe472a14b7d3c135c052f81
Reioo <reio.meiusi@gmail.com>
2023-02-02 19:19:54 +0200

    testing sort function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldest = arr.sort((a, b) => {
+        console.log(`a: ${a.yearOfBirth} \n b: ${b.yearOfBirth}`);
+    })
 };
 
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+ findTheOldest(people);
 // Do not edit below this line
 module.exports = findTheOldest;

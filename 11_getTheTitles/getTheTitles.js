e35ead96e0d2b819989d5df1077989b03d36ff62
Reioo <reio.meiusi@gmail.com>
2023-02-02 17:07:19 +0200

    Finished function to get titles from books

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,26 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = books.map(book => book.title)
+    console.log(titles);
+    removeWhiteSpaceFromArray(titles);
+    console.log(titles);
+    return titles;
 };
+
+function removeWhiteSpaceFromArray(array){
+    return array.filter((item) => item != ' ');
+}
+
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+];
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;

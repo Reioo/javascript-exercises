32edd5d9af16c70c3a0ff19d6124b245f3cec382
Reioo <reio.meiusi@gmail.com>
2023-02-02 16:25:30 +0200

    Finished palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,27 @@
-const palindromes = function () {
-
-};
-
-// Do not edit below this line
-module.exports = palindromes;
-
 function removePunctuation(str) {
     const punctuationless = str
       .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
       .replace(/\s{2,}/g, " ")
       .replace(/\s/g, '');
-      return punctuationless
+      return punctuationless;
 }
 
-const s = "This., -/ is #! an $ % ^ & * example ;: {} of a = -_ string with `~)() punctuation";
-console.log(removePunctuation(s));
+const palindromes = function (str) {
+    console.log(str.toLowerCase());
+
+    let copyOfStr = str.toLowerCase();
+    copyOfStr = removePunctuation(copyOfStr);
+    const halfString = copyOfStr.length / 2;
+    for(let i = 0; i < str.length; i++) {
+        if(copyOfStr[i] !== copyOfStr[copyOfStr.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Do not edit below this line
+module.exports = palindromes;
+
+
+

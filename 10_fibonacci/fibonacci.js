e9d1b41f410cc928eb5fc39b73edb3a0664892cd
Reioo <reio.meiusi@gmail.com>
2023-02-02 16:49:59 +0200

    Fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }
+    let numArray = []; 
+    for(let i = 0; i < number; i++) {
+        if(i < 2) {
+            numArray[i] = 1;
+        } else {
+            numArray[i] = numArray[i - 1] + numArray[i - 2];
+        }
+    }
+    return numArray[number - 1];
 };
 
 // Do not edit below this line

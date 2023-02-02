d831b63c8d345f010bcb47809e05acbb30978f62
Reioo <reio.meiusi@gmail.com>
2023-02-02 20:05:56 +0200

    Added if not dead, use current year

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,31 @@
 function sortPeopleByAge(arr) {
     const oldest = arr.sort((a, b) => {
-        const nextGuy = a.yearOfDeath - a.yearOfBirth;
-        const lastGuy = b.yearOfDeath - b.yearOfBirth;
+        const currentYear = new Date().getFullYear();
+        let nextGuy;
+        let lastGuy;
+        if(isNaN(a.yearOfDeath)) {
+            nextGuy = currentYear - a.yearOfBirth;   
+            console.log(nextGuy);
+        } else {
+            nextGuy = a.yearOfDeath - a.yearOfBirth;   
+            console.log(nextGuy);
+        }
+        if(isNaN(b.yearOfDeath)) {
+            lastGuy = currentYear - b.yearOfBirth;   
+            console.log(lastGuy);
+        } else {
+            lastGuy = b.yearOfDeath - b.yearOfBirth;   
+            console.log(lastGuy);
+        }
         return (lastGuy < nextGuy) ? -1 : 1;
     })
     return oldest;
 }
 
 const findTheOldest = function(arr) {
+    console.log(arr[0].yearOfDeath);
+    
+
     const sortedByAge = sortPeopleByAge(arr);
     return sortedByAge[0].name;
 };
@@ -16,7 +34,6 @@ const people = [
     {
       name: "Carly",
       yearOfBirth: 1942,
-      yearOfDeath: 1970,
     },
     {
       name: "Ray",

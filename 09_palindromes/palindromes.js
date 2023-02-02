5e735e0517a225a25ed73fb7047cbf20b88c32fb
Reioo <reio.meiusi@gmail.com>
2023-02-02 15:35:25 +0200

    Add function to remove punctuation and spaces

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -4,3 +4,14 @@ const palindromes = function () {
 
 // Do not edit below this line
 module.exports = palindromes;
+
+function removePunctuation(str) {
+    const punctuationless = str
+      .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+      .replace(/\s{2,}/g, " ")
+      .replace(/\s/g, '');
+      return punctuationless
+}
+
+const s = "This., -/ is #! an $ % ^ & * example ;: {} of a = -_ string with `~)() punctuation";
+console.log(removePunctuation(s));
